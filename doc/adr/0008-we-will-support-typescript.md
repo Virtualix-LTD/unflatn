883636873659fb6ab1d8d1e05cf05a2d39455999
Tony Klinakis <aklin@users.noreply.github.com>
2025-08-22 23:36:40 +0100

    Add typescript bindings

---
+++ doc/adr/0008-we-will-support-typescript.md
@@ -0,0 +1,16 @@
+# 8. We will support Typescript
+
+Date: 2025-08-22
+
+## Status
+
+Accepted
+
+## Context
+The issue motivating this decision, and any context that influences or constrains the decision.
+
+## Decision
+The change that we're proposing or have agreed to implement.
+
+## Consequences
+What becomes easier or more difficult to do and any risks introduced by the change that will need to be mitigated.
---
+++ lib/index.d.ts
@@ -0,0 +1,22 @@
+type UnflattenOpts = {
+  separator?: string;
+  objectMode?: boolean;
+}
+
+export = unflatten;
+
+/**
+ * @id unflatten
+ * @function unflatten
+ * Opposite of `flatten-obj`. Unflattens an object with delimited keys
+ * @param  {object} obj - Object that needs to be unflattened
+ * @param  {object|string|boolean} [opts] - Optional.
+ * - Provide a string as a shortcut for `{ separator: opts }`
+ * - Provide a boolean as a shorcut for `{ objectMode: opts }`
+ * - Provide an object to set both options `{ separator: '/', objectMode: true }`
+ * - Available options:
+ *   + **separator** (*string*) - defaults to `'.'`
+ *   + **objectMode** (*boolean*) - defaults to `false`
+ * @return {object} obj - Nested Javascript object
+ */
+declare function unflatten(obj: object, opts: string | boolean | UnflattenOpts): {}
